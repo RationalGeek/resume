ce6ce21f468ac9872a606ee0743237ffac52c1b3
RationalGeek <jkohlhepp@gmail.com>
2014-12-30 12:52:53 -0500

    grunt-gh-pages initial config

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // pkg: grunt.file.readJSON('package.json'),
+    // uglify: {
+      // options: {
+        // banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      // },
+      // build: {
+        // src: 'src/<%= pkg.name %>.js',
+        // dest: 'build/<%= pkg.name %>.min.js'
+      // }
+    // }
+	
+	'gh-pages': {
+		options: {
+		  base: 'dist'
+		},
+		src: ['**']
+	  }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  // Default task(s).
+  // grunt.registerTask('default', ['uglify']);
+
+};
