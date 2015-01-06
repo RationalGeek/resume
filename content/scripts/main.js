63579d4cc5bff5c8c802ab1c73a3baa9b189265d
RationalGeek <jkohlhepp@gmail.com>
2015-01-06 14:56:11 -0500

    Split content sections into separate files

--- content/scripts/main.js
+++ content/scripts/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+  // Insert nav items based on major sections
+  var mainnav = $('#mainnav > ul');
+  $('.majorsection').each(function(index, element) {
+      var sectionName = $(element).text();
+      var sectionId = $(element).attr('id');
+      mainnav.append('<li><a href=#' + sectionId + '>' + sectionName + '</a></li>');
+  });
+
   $('#mainnav').affix({
     offset: {
       top: 136
